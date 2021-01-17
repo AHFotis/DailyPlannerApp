239cfb018f2458576e274ec204a5de2bacbf8092
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-01-16 20:29:56 -0600

    CSS Cleanup

--- Develop/function.js
+++ Develop/function.js
@@ -1,15 +1,17 @@
-var content = $(".plannerContent");
-console.log(content);
 
+//Returns the current hour in military time
   var now = moment().hour();
   console.log(now);
 
+//Returns the date with day, month, date, year
   var date = moment().format("dddd, MMMM Do YYYY");
   console.log(date);
-
+//prints to HTML
   $("#currentDay").text(date);
 
-
+//variable for colorChange function
+  var content = $(".plannerContent");
+  console.log(content);
 //Function to change color blocks as day progresses
   function colorChange () {
 
@@ -31,3 +33,5 @@ console.log(content);
     }
   }
   colorChange();
+
+  
