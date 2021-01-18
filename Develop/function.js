92dfb38aa76595f713b7451fe01b869d2ca256b8
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-01-18 13:19:10 -0600

    Clear Button Functional and Code Clean

--- Develop/function.js
+++ Develop/function.js
@@ -1,89 +1,84 @@
 //Array to print to do list on refresh
 var itemsArray = [];
+//variable for colorChange and renderToDo functions
+var content = $(".plannerContent");
 
 //Returns the current hour in military time
-  var now = moment().hour();
+var now = moment().hour();
 
 //Returns the date with day, month, date, year
-  var date = moment().format("dddd, MMMM Do YYYY");
+var date = moment().format("dddd, MMMM Do YYYY");
 
 //prints to HTML
-  $("#currentDay").text(date);
+$("#currentDay").text(date);
 
-//variable for colorChange and renderToDo functions
-  var content = $(".plannerContent");
+//Clears page and local storage for a fresh day
+$(".clearBtn").on("click", function () {
 
-// Not working and not necessary
-  // function clearToDo () {
-  //     if (now === 1) {
-  //         localStorage.removeItem("9");
-  //         localStorage.removeItem("10");
-  //         localStorage.removeItem("11");
-  //         localStorage.removeItem("12");
-  //         localStorage.removeItem("13");
-  //         localStorage.removeItem("14");
-  //         localStorage.removeItem("15");
-  //         localStorage.removeItem("16");
-  //         localStorage.removeItem("17");
-  //     }
-  // }
-  // clearToDo();
+  $(".plannerContent").html("");
 
-  function getToDos () {
-    for (var j = 9; j < 18; j++) {
-      
-      var items = localStorage.getItem(j);
-      console.log(items);
-      itemsArray.push(items);
-      }
+  for (var x = 9; x < 18; x++) {
+
+    localStorage.removeItem(x);
+
   }
-  
-  getToDos();
-  console.log(itemsArray)
+})
 
-  function renderToDo (items) {
-  
-    for (var k = 0; k < content.length; k++) {
-        console.log(content[k]);
-        
-        if (items[k] !== null) {
-        content[k].innerHTML = items[k];
-        }
-      }
+//Gets the list of to dos from local storage on refresh
+function getToDos() {
+  for (var j = 9; j < 18; j++) {
+
+    var items = localStorage.getItem(j);
+    console.log(items);
+    itemsArray.push(items);
+  }
+}
+getToDos();
+console.log(itemsArray)
+
+//Renders to dos to the page
+function renderToDo(arr) {
+
+  for (var k = 0; k < content.length; k++) {
+    console.log(content[k]);
+
+    if (arr[k] !== null) {
+      content[k].innerHTML = arr[k];
     }
-     
+  }
+}
 renderToDo(itemsArray);
- 
 
 
 //Function to change color blocks as day progresses
-  function colorChange () {
+function colorChange() {
 
-    for (var i = 0; i < content.length; i++) {
-     var divNum = parseInt(content[i].dataset.time);
-     console.log(divNum)
-     
-     if (divNum > now) {
-     content[i].setAttribute("id", "future");
-     }
+  for (var i = 0; i < content.length; i++) {
+    var divNum = parseInt(content[i].dataset.time);
+    console.log(divNum)
 
-     if (divNum == now) {
+    if (divNum > now) {
+      content[i].setAttribute("id", "future");
+    }
+
+    if (divNum == now) {
       content[i].setAttribute("id", "present");
-     }
+    }
 
-     if (divNum < now) {
+    if (divNum < now) {
       content[i].setAttribute("id", "past");
-     }
     }
   }
-  colorChange();
+}
+colorChange();
 
+//Saves to do item to local storage with save button is clicked
 $(".saveBtn").on("click", function () {
-    var relatedInput = $(this).parent().children(".plannerContent").val();
-    var saveName = $(this).data("time");
-    console.log(relatedInput)
+  var relatedInput = $(this).parent().children(".plannerContent").val();
+  var saveName = $(this).data("time");
+  console.log(relatedInput)
 
-    localStorage.setItem(saveName, relatedInput);
+  localStorage.setItem(saveName, relatedInput);
 })
 
 
