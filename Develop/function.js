5ef7b14940071394258e76fedfbfb65ebfc03c0d
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-01-21 14:14:36 -0600

    Full Application Code Clean

--- Develop/function.js
+++ Develop/function.js
@@ -1,6 +1,6 @@
 //Array to print to do list on refresh
 var itemsArray = [];
-//variable for colorChange and renderToDo functions
+//Variable for colorChange and renderToDo functions
 var content = $(".plannerContent");
 
 //Returns the current hour in military time
@@ -9,7 +9,7 @@ var now = moment().hour();
 //Returns the date with day, month, date, year
 var date = moment().format("dddd, MMMM Do YYYY");
 
-//prints to HTML
+//Prints date to HTML
 $("#currentDay").text(date);
 
 //Clears page and local storage for a fresh day
@@ -18,30 +18,24 @@ $(".clearBtn").on("click", function () {
   $(".plannerContent").html("");
 
   for (var x = 9; x < 18; x++) {
-
     localStorage.removeItem(x);
-
   }
 })
 
 //Gets the list of to dos from local storage on refresh
 function getToDos() {
   for (var j = 9; j < 18; j++) {
-
     var items = localStorage.getItem(j);
-    console.log(items);
     itemsArray.push(items);
   }
 }
 getToDos();
-console.log(itemsArray)
+
 
 //Renders to dos to the page
 function renderToDo(arr) {
 
   for (var k = 0; k < content.length; k++) {
-    console.log(content[k]);
-
     if (arr[k] !== null) {
       content[k].innerHTML = arr[k];
     }
@@ -55,7 +49,6 @@ function colorChange() {
 
   for (var i = 0; i < content.length; i++) {
     var divNum = parseInt(content[i].dataset.time);
-    console.log(divNum)
 
     if (divNum > now) {
       content[i].setAttribute("id", "future");
@@ -72,14 +65,12 @@ function colorChange() {
 }
 colorChange();
 
-//Saves to do item to local storage with save button is clicked
+//Saves to do item to local storage when save button is clicked
 $(".saveBtn").on("click", function () {
 
   var relatedInput = $(this).parent().children(".plannerContent").val();
   var saveName = $(this).data("time");
-  console.log(saveName)
 
   localStorage.setItem(saveName, relatedInput);
+
 })
-
-
